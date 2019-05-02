caf56316290eb990a581769ae1c9a11cf18eebc4
newsear <zyncgx@hotmail.com>
2019-05-03 00:29:01 +0700

    add Provider

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
 
 import App from './containers/App'
 import * as serviceWorker from './serviceWorker'
+import store from './utils/store'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+const Root = () => (
+  <Provider store={store}>
+    <App />
+  </Provider>
+)
+
+ReactDOM.render(<Root />, document.getElementById('root'))
 
 serviceWorker.unregister()
