ef666a9b04128bc047ba90a2f931f4aa257226bb
newsear <zyncgx@hotmail.com>
2019-05-12 17:18:51 +0700

    route in App

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -1,8 +1,25 @@
 import React from 'react'
+import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom'
+
+import PrivateRoute from '../../components/PrivateRoute'
+import Login from '../Login'
+import Home from '../Home'
+import NotFound from '../NotFound'
+
+import 'antd/dist/antd.css'
+import './index.css'
 
 function App() {
   return (
-    <p>Hello World!</p>
+    <BrowserRouter>
+      <Switch>
+        <Route path='/login' exact component={Login} />
+        <PrivateRoute path='/home' exact component={Home} />
+        <Route path='/not-found' exact component={NotFound} />
+        <Redirect from='/' to='/home' exact />
+        <Redirect to='/not-found' />
+      </Switch>
+    </BrowserRouter>
   )
 }
 
