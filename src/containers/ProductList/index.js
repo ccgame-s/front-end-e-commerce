4f4228a4e077148f4bd874ef41c441f191320b17
newsear <zyncgx@hotmail.com>
2019-05-18 20:34:15 +0700

    display products

--- src/containers/ProductList/index.js
+++ src/containers/ProductList/index.js
@@ -1,11 +1,66 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React, { Component } from 'react'
+import { Table, Divider } from 'antd'
+import { connect } from 'react-redux'
+import PropTypes from 'prop-types'
 
 class ProductList extends Component {
+  columns = [
+    {
+      title: 'Name',
+      dataIndex: 'name',
+      key: 'name',
+    },
+    {
+      title: 'Price',
+      dataIndex: 'price',
+      key: 'price',
+    },
+    {
+      title: 'Action',
+      key: 'action',
+      render: () => (
+        <span>
+          <a onClick={this.onEdit}>Edit</a>
+          <Divider type="vertical" />
+          <a onClick={this.onDelete} style={{ color: 'red' }}>Delete</a>
+        </span>
+      ),
+    },
+  ]
+
+  componentDidMount() {
+    const { fetchProducts } = this.props
+    fetchProducts()
+  }
+
+  onEdit = () => {
+    alert('Edit')
+  }
+
+  onDelete = () => {
+    alert('delete')
+  }
+  
   render() {
+    const { products } = this.props
     return (
-      <h1>This is ProductList</h1>
+      <Table dataSource={products} columns={this.columns} rowKey='_id' />
     )
   }
 }
 
-export default ProductList
+ProductList.propTypes = {
+  products: PropTypes.array,
+  fetchProducts: PropTypes.func
+}
+
+const mapState = state => ({
+  products: state.Products.products
+})
+
+const mapDispatch = dispatch => ({
+  fetchProducts: dispatch.Products.fetchProducts
+})
+
+export default connect(mapState, mapDispatch)(ProductList)
