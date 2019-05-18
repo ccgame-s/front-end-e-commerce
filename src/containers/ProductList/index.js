999890ec17bbdb3942edf6bfd09d6aa560810dda
newsear <zyncgx@hotmail.com>
2019-05-18 15:10:39 +0700

    add product list page

---
+++ src/containers/ProductList/index.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+class ProductList extends Component {
+  render() {
+    return (
+      <h1>This is ProductList</h1>
+    )
+  }
+}
+
+export default ProductList
