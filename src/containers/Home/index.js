66d778d4ecf41c1e285130db3f7e9e33f67d75f5
newsear <zyncgx@hotmail.com>
2019-05-18 15:11:09 +0700

    display component in home

--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -1,8 +1,12 @@
-import React, { Component } from 'react'
-import { Layout, Menu, Icon } from 'antd'
+import React, { PureComponent } from 'react'
+import { Layout, Menu, Icon, Typography } from 'antd'
 import styled from 'styled-components'
 
+import Logout from '../Logout'
+import ProductList from '../ProductList'
+
 const { Content, Footer, Sider } = Layout
+const { Title } = Typography
 
 const Logo = styled.div`
   background-color: #002140;
@@ -13,28 +17,48 @@ const Logo = styled.div`
   justify-content: center;
 `
 
+const Background = styled.div`
+  padding: 24px;
+  background-color: #fff;
+  min-height: 360px;
+  ${({ center }) => center && `
+    display: flex;
+    justify-content: center;
+    align-items: center;`
+  }
+`
+
 const menuItems = [
   {
     key: 'home',
     icon: 'home',
-    label: 'Home',
+    label: 'Home'
+  },
+  {
+    key: 'products',
+    icon: 'unordered-list',
+    label: 'Products',
+    component: ProductList
   },
   {
     key: 'logout',
     icon: 'logout',
     label: 'Logout',
-    link: '/logout'
+    component: Logout
   }
 ]
 
-class Home extends Component {
-  gotoLink = link => {
-    if(link) {
-      window.location = link
-    }
+class Home extends PureComponent {
+  state = {
+    Component: null
+  }
+
+  onMenuClick = (component) => {
+    this.setState({ Component: component })
   }
 
   render() {
+    const { Component } = this.state
     return (
       <Layout style={{ minHeight: '100vh' }}>
         <Sider>
@@ -42,7 +66,7 @@ class Home extends Component {
           <Menu theme='dark' defaultSelectedKeys={['home']}>
             {
               menuItems.map(item => (
-                <Menu.Item key={item.key} onClick={() => this.gotoLink(item.link)}>
+                <Menu.Item key={item.key} onClick={() => this.onMenuClick(item.component)}>
                   <Icon type={item.icon}/>
                   <span>{item.label}</span>
                 </Menu.Item>
@@ -52,9 +76,17 @@ class Home extends Component {
         </Sider>
         <Layout>
           <Content style={{ margin: 16 }}>
-            <div style={{ padding: 24, background: '#fff', minHeight: 360 }}>
-              Bill is a cat.
-            </div>
+            {
+              Component ? (
+                <Background>
+                  <Component />
+                </Background>
+              ) : (
+                <Background center>
+                  <Title level={2}>Welcome to E-Commerce Admin</Title>
+                </Background>
+              )
+            }
           </Content>
           <Footer style={{ textAlign: 'center' }}>Created by ccgame</Footer>
         </Layout>
